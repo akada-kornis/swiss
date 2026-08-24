bb3a2c7398eefecaf22121cbf215cbb24bba905a
AxelK <obersax@gmail.com>
2026-08-24 15:16:37 -0700

    Add invoiced eAdmin clients

--- cities/scripts/correct-ge-siacg.mjs
+++ cities/scripts/correct-ge-siacg.mjs
@@ -4,7 +4,7 @@ const file = new URL("../public/data/municipalities.json", import.meta.url);
 const data = JSON.parse(fs.readFileSync(file, "utf8"));
 const eAdminMunicipalities = new Map([
   ["GE", new Set(["Lancy", "Vernier", "Thônex", "Onex", "Carouge (GE)", "Plan-les-Ouates", "Collonge-Bellerive", "Cologny"])],
-  ["VD", new Set(["Montreux", "Vevey", "Aubonne", "Prangins", "Rolle", "Cossonay", "Echandens", "Ecublens (VD)", "Gland", "Le Mont-sur-Lausanne"])],
+  ["VD", new Set(["Montreux", "Vevey", "Aubonne", "Prangins", "Rolle", "Cossonay", "Echandens", "Ecublens (VD)", "Gland", "Le Mont-sur-Lausanne", "Bourg-en-Lavaux", "Blonay - Saint-Légier"])],
 ]);
 
 for (const municipality of data.municipalities) {
